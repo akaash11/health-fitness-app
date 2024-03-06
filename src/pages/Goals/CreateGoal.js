124c3d6c1b3c0a766bb633a87213f3c03b1d1cff
Akaash Trivedi <akaash.t1@gmail.com>
2024-03-05 16:44:50 -0800

    Validate goal inputs

--- src/pages/Goals/CreateGoal.js
+++ src/pages/Goals/CreateGoal.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { createTheme,ThemeProvider, Container, CssBaseline, Box, Typography, Grid, TextField, Button, MenuItem, FormControl, InputLabel, Select, Avatar } from '@mui/material';
+import { createTheme,ThemeProvider, Container, CssBaseline, Box, Typography, Grid, TextField, Button, MenuItem, FormControl, InputLabel, Select, Avatar, FormHelperText } from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined'; // Ensure this icon is imported
 
 const defaultTheme = createTheme();
@@ -9,14 +9,27 @@ const CreateGoal = () => {
   const [activity, setActivity] = useState('');
   const [goalType, setGoalType] = useState('');
   const [goalTarget, setGoalTarget] = useState('');
+  const [errors, setErrors] = useState({});
+
+  const validateForm = () => {
+    let tempErrors = {};
+    tempErrors.goal = goal ? "" : "This field is required.";
+    tempErrors.activity = activity ? "" : "This field is required.";
+    tempErrors.goalType = goalType ? "" : "This field is required.";
+    tempErrors.goalTarget = goalTarget ? "" : "This field is required.";
+    setErrors(tempErrors);
+    return Object.values(tempErrors).every(x => x === "");
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log({ goal, activity, goalType, goalTarget });
+    if (validateForm()) {
+      console.log({ goal, activity, goalType, goalTarget });
+      // Here, you can proceed with form submission, such as sending data to a server.
+    }
   };
-
-  return (
-    <ThemeProvider theme={defaultTheme}>
+  return(
+  <ThemeProvider theme={defaultTheme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <Box
@@ -36,44 +49,50 @@ const CreateGoal = () => {
           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
               <Grid item xs={12}>
-                <FormControl fullWidth>
+                <FormControl fullWidth error={Boolean(errors.goal)}>
                   <InputLabel>Goals</InputLabel>
                   <Select
                     value={goal}
                     label="Goals"
                     onChange={(e) => setGoal(e.target.value)}
+                    required
                   >
                     <MenuItem value="loseWeight">Lose Weight</MenuItem>
                     <MenuItem value="buildMuscle">Build Muscle</MenuItem>
                     <MenuItem value="improveEndurance">Improve Endurance</MenuItem>
                   </Select>
+                  {errors.goal && <FormHelperText>{errors.goal}</FormHelperText>}
                 </FormControl>
               </Grid>
               <Grid item xs={12}>
-                <FormControl fullWidth>
+                <FormControl fullWidth error={Boolean(errors.activity)}>
                   <InputLabel>Activity</InputLabel>
                   <Select
                     value={activity}
                     label="Activity"
                     onChange={(e) => setActivity(e.target.value)}
+                    required
                   >
                     <MenuItem value="run">Run</MenuItem>
                     <MenuItem value="walk">Walk</MenuItem>
                     <MenuItem value="ride">Ride</MenuItem>
                   </Select>
+                  {errors.activity && <FormHelperText>{errors.activity}</FormHelperText>}
                 </FormControl>
               </Grid>
               <Grid item xs={12}>
-                <FormControl fullWidth>
+                <FormControl fullWidth error={Boolean(errors.goalType)}>
                   <InputLabel>Goal Type</InputLabel>
                   <Select
                     value={goalType}
                     label="Goal Type"
                     onChange={(e) => setGoalType(e.target.value)}
+                    required
                   >
                     <MenuItem value="distance">Distance</MenuItem>
                     <MenuItem value="duration">Duration</MenuItem>
                   </Select>
+                  {errors.goalType && <FormHelperText>{errors.goalType}</FormHelperText>}
                 </FormControl>
               </Grid>
               <Grid item xs={12}>
@@ -83,6 +102,9 @@ const CreateGoal = () => {
                   value={goalTarget}
                   onChange={(e) => setGoalTarget(e.target.value)}
                   type="number"
+                  error={Boolean(errors.goalTarget)}
+                  helperText={errors.goalTarget}
+                  required
                 />
               </Grid>
               <Grid item xs={12}>
