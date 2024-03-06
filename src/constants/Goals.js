a0e92496af02b06be0258a9d7793b69ed66df0a7
Akaash Trivedi <akaash.t1@gmail.com>
2024-03-05 17:59:24 -0800

    dummy goals

---
+++ src/constants/Goals.js
@@ -0,0 +1,42 @@
+export const goals = [
+    {
+      "id": 6779,
+      "activity": "Walk",
+      "goalType": "mi",
+      "goalTarget": 3.5,
+      "progress": 2.9,
+      "daysLeft": 3
+    },
+    {
+      "id": 6373,
+      "activity": "Walk",
+      "goalType": "mi",
+      "goalTarget": 8.5,
+      "progress": 7.3,
+      "daysLeft": 6
+    },
+    {
+      "id": 3782,
+      "activity": "Run",
+      "goalType": "hour",
+      "goalTarget": 9.6,
+      "progress": 4.8,
+      "daysLeft": 1
+    },
+    {
+      "id": 2518,
+      "activity": "Run",
+      "goalType": "hour",
+      "goalTarget": 1.9,
+      "progress": 1.7,
+      "daysLeft": 2
+    },
+    {
+      "id": 5919,
+      "activity": "Run",
+      "goalType": "hour",
+      "goalTarget": 5.6,
+      "progress": 0.5,
+      "daysLeft": 2
+    }
+  ]
